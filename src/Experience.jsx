730f6e0682d42814311047751c979b60881a6c12
Josiah Webb <jj.webb520@gmail.com>
2023-01-27 17:25:37 -0700

    more pointer event notes, performance

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,19 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { meshBounds, OrbitControls, useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
+
+/**
+ * General pointer events notes
+ * - avoid events that need testing on each frame
+ *      e.g. move, over, enter, out, leave
+ *      these are taxing on cpu
+ * - minimize num of obj that listen to events
+ * - avoid testing complex geometries
+ * - any freeze or jumps in animation is good indication that perf needs work
+ * - meshBounds: good resource to testing complex geos
+ *      it creates simple testing geo around object with far less polygons to test
+ *      drawback - much less precise, pointer events might not be exact to object, only works on single meshes
+ */
 
 export default function Experience()
 {
@@ -17,6 +30,8 @@ export default function Experience()
         cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`) // lesson
     }
 
+    const { scene } = useGLTF('./hamburger.glb')
+
     return <>
 
         <OrbitControls makeDefault />
@@ -27,6 +42,8 @@ export default function Experience()
         <mesh
             position-x={ - 2 }
             onClick={(e) => e.stopPropagation()} // this is useful to prevent the onClick of the cube form happening, e.g. if sphere infront of cube and don't want cube click event to fire in this case
+            onPointerEnter={(e) => e.stopPropagation()}
+            onPointerLeave={(e) => e.stopPropagation()}
         >
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
@@ -34,10 +51,12 @@ export default function Experience()
 
         <mesh
             ref={ cube }
+            raycast={ meshBounds }
             position-x={ 2 }
             scale={ 1.5 }
             onClick={ handleClick }
-            onPointerEnter={(e) => }
+            onPointerEnter={() => document.body.style.cursor = 'pointer'}
+            onPointerLeave={() => document.body.style.cursor = 'default'}
         >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
@@ -47,6 +66,13 @@ export default function Experience()
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <primitive object={ scene } scale={ .35 } position-y={ 1.5 } 
+            onClick={(e) => {
+                console.log('helooo', e.object.name)
+                e.stopPropagation()
+            }}
+        />
 
     </>
 }
